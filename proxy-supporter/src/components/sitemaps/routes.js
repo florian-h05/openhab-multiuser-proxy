e46023f92322a49c822cb08389d696e4b56d4305
Florian Hotze <florianh_dev@icloud.com>
2022-02-25 16:45:20 +0100

    Complete routes for sitemaps
    Signed-off-by: Florian Hotze <florianh_dev@icloud.com>

--- proxy-supporter/src/components/sitemaps/routes.js
+++ proxy-supporter/src/components/sitemaps/routes.js
@@ -1,6 +1,5 @@
 import fetch from 'node-fetch';
 import { backendInfo } from './../../server.js';
-import { backendError } from './../http-error.js';
 import { requireHeader } from './../middleware.js';
 
 /**
@@ -11,7 +10,7 @@ const sitemaps = (app) => {
    * @swagger
    * /rest/sitemaps:
    *   get:
-   *     summary: Get all available sitemaps for the given user/org.
+   *     summary: Get all available sitemaps.
    *     parameters:
    *       - in: header
    *         name: X-OPENHAB-USER
@@ -40,10 +39,9 @@ const sitemaps = (app) => {
    *               items:
    *                 type: object
    */
-  app.get('/rest/sitemaps', requireHeader('X-OPENHAB-USER'), async (req, res) => {
-    const org = await req.headers['x-openhab-org'] || [];
-    const user = await req.headers['x-openhab-user'];
-    const url = await backendInfo.HOST + '/rest/sitemaps';
+  app.get('/rest/sitemaps', requireHeader('X-OPENHAB-USER'), (req, res) => {
+    const org = req.headers['x-openhab-org'] || [];
+    const user = req.headers['x-openhab-user'];
 
     fetch(backendInfo.HOST + '/rest/sitemaps')
       .then((res) => res.json()) // openHAB returns a JSON array of objects
@@ -57,7 +55,68 @@ const sitemaps = (app) => {
         console.debug(`Received HTTP GET request from IP [${req.ip}] at [${req.url}]`);
         res.status(200).send(sitemaps);
       })
-      .catch((err) => backendError(url, err, res));
+      .catch((err) => {
+        console.error(`An error occurred when requesting backend [${req.url}]: ${err}`);
+        res.status(404).send();
+      });
+  });
+
+  /**
+   * @swagger
+   * /rest/sitemaps/{sitemapname}:
+   *   get:
+   *     summary: Get sitemap by name.
+   *     parameters:
+   *       - in: path
+   *         name: sitemapname
+   *         required: true
+   *         description: Sitemap name.
+   *         schema:
+   *           type: string
+   *         style: form
+   *       - in: header
+   *         name: X-OPENHAB-USER
+   *         required: true
+   *         description: Name of user.
+   *         schema:
+   *           type: string
+   *         style: form
+   *       - in: header
+   *         name: X-OPENHAB-ORG
+   *         required: false
+   *         description: Organisations the user is member of.
+   *         schema:
+   *           type: array
+   *           items:
+   *             type: string
+   *         style: form
+   *         explode: true
+   *     responses:
+   *       200:
+   *         description: OK
+   *         content:
+   *           application/json:
+   *             schema:
+   *               type: object
+   */
+  app.get('/rest/sitemaps/:sitemapname', requireHeader('X-OPENHAB-USER'), (req, res) => {
+    const org = req.headers['x-openhab-org'] || [];
+    const user = req.headers['x-openhab-user'];
+
+    if (req.params.sitemapname === user || org.includes(req.params.sitemapname)) {
+      fetch(backendInfo.HOST + '/rest/sitemaps/' + req.params.sitemapname)
+        .then((res) => res.json())
+        .then((json) => {
+          console.debug(`Received HTTP GET request from IP [${req.ip}] at [${req.url}]`);
+          res.status(200).send(json);
+        })
+        .catch((err) => {
+          console.error(`An error occurred when requesting backend [${req.url}]: ${err}`);
+          res.status(404).send();
+        });
+    } else {
+      res.status(404).send();
+    }
   });
 };
 
